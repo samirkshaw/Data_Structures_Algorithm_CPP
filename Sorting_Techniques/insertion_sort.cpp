5d36693d06563c2ecb8829722ab4fe74799820f3
Samir kumar shaw <samir.kshaw2005@gmail.com>
2026-06-09 08:54:04 +0530

    Learnt sorting by comparing and shifting elements using insertion sort technique

---
+++ Sorting_Techniques/insertion_sort.cpp
@@ -0,0 +1,34 @@
+// Problem  : Insertion Sort
+// Approach : Pick each element as key, shift larger sorted elements right, insert
+// Time     : O(n^2)
+// Space    : O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+void insertionSort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+}
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    insertionSort(arr);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
